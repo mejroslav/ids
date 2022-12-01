3fa06d6b61dc79d2cfbb0acdb697ac3838d31bee
m93a <m93a.cz@gmail.com>
2022-12-01 15:37:26 +0100

    add more info to Item

--- src/sources/types.ts
+++ src/sources/types.ts
@@ -1,6 +1,13 @@
 export interface Item {
     title: string;
     content: string;
+    author: Author;
+    published: Date;
+}
+
+export interface Author {
+    profileImage: URL;
+    title: string;
 }
 
 export interface StreamOptions {
